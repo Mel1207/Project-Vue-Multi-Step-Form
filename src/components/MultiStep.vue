4cf72bf2d4076247d1d9f924929de66ceacbc9d5
Mel <meljohnpualon@gmail.com>
2023-08-16 16:58:04 +0800

    created step card basic ui setup

--- src/components/MultiStep.vue
+++ src/components/MultiStep.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="card">
-    
+    <div class="steps">
+
+    </div>
+    <div class="step-form-details">
+      <h1>Personal Info</h1>
+      <p>Please provide your name, email and phone number.</p>
+    </div>
   </div>
 </template>
 
