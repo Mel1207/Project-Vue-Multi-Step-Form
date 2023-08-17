593c0642032668710d5bc7aec58fb27f6745a506
Mel <meljohnpualon@gmail.com>
2023-08-17 17:20:20 +0800

    added step component

--- src/components/MultiStep.vue
+++ src/components/MultiStep.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="card">
-    <div class="steps">
-
-    </div>
+    <Steps v-for="step in steps" :key="step.stepNumber"/>
     <div class="step-form-details">
       <h1>Personal Info</h1>
       <p>Please provide your name, email and phone number.</p>
@@ -11,5 +9,13 @@
 </template>
 
 <script setup>
+import Steps from './Steps.vue'
+import { ref } from 'vue';
+
+const steps = ref([
+  { stepNumber: 1, },
+  { stepTitle: 'Step 1'},
+  { stepDetail: 'Your Info'}
+])
 
 </script>
---
+++ src/components/Steps.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="steps">
+    <div class="dot">1</div>
+    <div class="dot-details">
+      <span>Step 1</span>
+      <p>Your Info</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
