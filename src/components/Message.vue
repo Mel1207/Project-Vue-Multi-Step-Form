6519bf4d5148f2b6e55cb25df7bf57030c66ce12
Mel <meljohnpualon@gmail.com>
2023-08-20 09:26:50 +0800

    added thank you icon

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <img src="../assets/icon-thank-you.svg" alt="Thank you">
     <h1>Thank you</h1>
     <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com</p>
   </div>
