3f8b4a9e3a5109479d2b3da5b0c692e56a3cb02a
Mel <meljohnpualon@gmail.com>
2023-08-22 16:06:20 +0800

    added radio buttons

--- src/components/PlansInfo.vue
+++ src/components/PlansInfo.vue
@@ -1,10 +1,39 @@
 <template>
   <h1>Select your plan</h1>
   <p>You have the option of monthly or yearly billing</p>
-  <button @click="$store.state.currentStep--">Go Back</button>
-  <button @click="$store.state.currentStep++">Next</button>
+  <br>
+  
+  <form @submit="validateForm">
+    <div class="form-group">
+      <label for="arcade">test</label>
+      <input type="radio" id="arcade" name="promo">
+      <label for="advanced">advanced</label>
+      <input type="radio" id="advanced" name="promo">
+      <label for="pro">pro</label>
+      <input type="radio" id="pro" name="promo">
+    </div>
+
+    <div class="form-group checkbox">
+      <input type="checkbox">
+    </div>
+  
+    <button>Next</button>
+    <button @click.self="goBack" type="button">Go Back</button>
+  </form>
+  
+  
 </template>
 
 <script setup>
+import store from '@/store';
+
+  const validateForm = () => {
+    console.log('validated')
+    store.state.currentStep++
+  }
+
+  const goBack = () => {
+    store.state.currentStep--
+  }
 
 </script>
