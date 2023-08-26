e64e6f152b68ee2963fdec78a45507a67189c5eb
Mel <meljohnpualon@gmail.com>
2023-08-27 03:21:08 +0800

    fix thank you message ui

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="message">
     <img src="../assets/icon-thank-you.svg" alt="Thank you">
-    <h1>Thank you</h1>
+    <h1>Thank you!</h1>
     <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com</p>
   </div>
 </template>
