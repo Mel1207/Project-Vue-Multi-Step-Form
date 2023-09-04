8996eed436c8553519e45e7b2f1489a7b26726cf
Mel <meljohnpualon@gmail.com>
2023-09-05 06:06:33 +0800

    fix card positioning

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <template>
+  <div class="steps-mobile">
+    <Step v-for="step in steps" :key="step.id" :stepProp="step"/>
+  </div>
   <MultiStep />
 </template>
 
 <script setup>
+import Step from './components/Steps.vue'
 import MultiStep from './components/MultiStep.vue'
+
+const steps = [
+  { 
+    stepNumber: 1, 
+    stepTitle: 'Step 1',
+    stepDetail: 'Your Info'
+  },
+  { 
+    stepNumber: 2, 
+    stepTitle: 'Step 2',
+    stepDetail: 'Select Plan'
+  },
+  { 
+    stepNumber: 3, 
+    stepTitle: 'Step 3',
+    stepDetail: 'Add-ons'
+  },
+  { 
+    stepNumber: 4, 
+    stepTitle: 'Step 4',
+    stepDetail: 'Summary'
+  }
+]
 
 </script>
